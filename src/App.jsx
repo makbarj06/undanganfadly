be7f4b331c05fa9e6a092f6626a1ff73077eab0b
akbaroit <makbarj06@gmail.com>
2024-08-19 07:23:17 +0800

    ..029

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import gmbr from "./img/LAMMING31.svg";
 function App() {
   return (
     <>
-      <h1>haloo</h1>
+      <h1 className="text-blue-500">haloo</h1>
       <img src={gmbr} alt="" />
     </>
   );
