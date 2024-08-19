6ed57b66a4e492a1a2766f78792762b5089ecfe3
akbaroit <makbarj06@gmail.com>
2024-08-19 18:05:33 +0800

    v02

--- src/component/Cover.jsx
+++ src/component/Cover.jsx
@@ -1,10 +1,30 @@
 import gmbr from "../img/LAMMING31.svg";
+import bckgrnd from "../img/background.svg";
+import lammingAtas from "../img/lammingatas.svg";
+import lammingBawah from "../img/lammingbawah.svg";
 
 export default function Cover() {
   return (
-    <div>
-      <h1>sxs</h1>
-      <img src={gmbr} alt="" />
+    <div className="w-screen h-screen xl:flex justify-center  overflow-hidden">
+      <div
+        className="w-full h-full xl:w-[390px] "
+        style={{
+          backgroundImage: `url(${bckgrnd})`,
+        }}
+      >
+        <div
+          className="h-[85%] w-full bg-repeat-x sm:bg-repeat-x bg-[50%,0%]"
+          style={{
+            backgroundImage: `url(${lammingAtas})`,
+          }}
+        ></div>
+        <div
+          className="h-[15%] bg-repeat-x bg-cover"
+          style={{
+            backgroundImage: `url(${lammingBawah})`,
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
