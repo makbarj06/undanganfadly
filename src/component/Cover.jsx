565657997481a4bbce519395c265d9064e968340
akbaroit <makbarj06@gmail.com>
2024-08-19 18:33:36 +0800

    v03

--- src/component/Cover.jsx
+++ src/component/Cover.jsx
@@ -1,9 +1,13 @@
 import gmbr from "../img/LAMMING31.svg";
 import bckgrnd from "../img/background.svg";
-import lammingAtas from "../img/lammingatas.svg";
-import lammingBawah from "../img/lammingbawah.svg";
+import lammingAtas from "../img/lammingAtas.png";
+import lammingBawah from "../img/lammingBawah.png";
+import badik from "../img/badik.svg";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export default function Cover() {
+  const [isBackgroundComplete, setIsBackgroundComplete] = useState(false);
   return (
     <div className="w-screen h-screen xl:flex justify-center  overflow-hidden">
       <div
@@ -12,18 +16,47 @@ export default function Cover() {
           backgroundImage: `url(${bckgrnd})`,
         }}
       >
-        <div
+        <motion.div
           className="h-[85%] w-full bg-repeat-x sm:bg-repeat-x bg-[50%,0%]"
           style={{
             backgroundImage: `url(${lammingAtas})`,
           }}
-        ></div>
-        <div
+          initial={{ y: "-100vh" }} // Start from off-screen (top)
+          animate={{ y: 0 }} // Move to original position
+          transition={{ type: "spring", stiffness: 50, delay: 0.2 }} // Spring effect with delay
+          onAnimationComplete={() => setIsBackgroundComplete(true)} // Set ke true saat animasi selesai
+        >
+          <div className="h-[12%] sm:h-[15%] xl:h-[12%] "></div>
+          <div className="h-[88%] sm:h-[85%] xl:h-[88%] ">
+            <div className="">
+              <div className="flex justify-center pt-3 ">
+                {isBackgroundComplete && (
+                  <motion.div
+                    className="flex justify-center pt-3"
+                    initial={{ x: "-100vw" }}
+                    animate={{ x: 0 }}
+                    transition={{ duration: 1 }}
+                  >
+                    <img
+                      src={badik}
+                      alt=""
+                      className="sm:scale-150 xl:scale-100 sm:pt-3"
+                    />
+                  </motion.div>
+                )}
+              </div>
+            </div>
+          </div>
+        </motion.div>
+        <motion.div
           className="h-[15%] bg-repeat-x bg-cover"
           style={{
             backgroundImage: `url(${lammingBawah})`,
           }}
-        ></div>
+          initial={{ y: "100%" }}
+          animate={{ y: "0%" }}
+          transition={{ duration: 1.5 }}
+        ></motion.div>
       </div>
     </div>
   );
